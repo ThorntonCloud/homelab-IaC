5fee1ebb7cdb971ab3dd69e230a909f00e68c94f
Jeremy Thornton <jeremy@thorntoncloud.com>
2025-10-21 14:35:48 -0400

    separated cluster deploy and argo bootstrap into separate workflows

--- Talos/terraform/providers.tf
+++ Talos/terraform/providers.tf
@@ -21,17 +21,5 @@ terraform {
       source  = "hashicorp/null"
       version = "~> 3.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 3.0.2"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.5.3"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.38.0"
-    }
   }
 }
